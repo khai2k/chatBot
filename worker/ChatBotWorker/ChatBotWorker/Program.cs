6c8fed9678466d3aab2b3cee5d948da853a2dc73
Chu Văn Hạnh <vanhanhf5@gmail.com>
2020-11-11 11:33:10 +0700

    uptumlum

--- worker/ChatBotWorker/ChatBotWorker/Program.cs
+++ worker/ChatBotWorker/ChatBotWorker/Program.cs
@@ -10,6 +10,21 @@ namespace ChatBotWorker
     {
         static void Main(string[] args)
         {
+
+            Task<bool> obTask = Task.Run(() => (
+
+                Program.ScheduleTask()
+
+            ));
+
+        }
+        public static bool ScheduleTask()
+        {
+            var ListPage = new List<long>();
+            ListPage.Add(124707154243128);
+
+
+            return true;
         }
     }
 }
