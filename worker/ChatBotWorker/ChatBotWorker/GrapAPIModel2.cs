6091fbb44dfe42f90014762eba6d44c66544a2bf
Chu Văn Hạnh <vanhanhf5@gmail.com>
2020-11-11 15:00:25 +0700

    upcode

---
+++ worker/ChatBotWorker/ChatBotWorker/GrapAPIModel2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBotWorker
+{
+    public class GrapAPIModel2
+    {
+    }
+
+
+    public class GrapAPIModel
+    {
+        public List<Datum> data { get; set; }
+        public Paging2 paging { get; set; }
+    }
+
+    public class Datum2
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Senders
+    {
+        public List<Datum2> data { get; set; }
+    }
+
+    public class From
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Datum4
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string id { get; set; }
+    }
+
+    public class To
+    {
+        public List<Datum4> data { get; set; }
+    }
+
+    public class Datum3
+    {
+        public string message { get; set; }
+        public DateTime created_time { get; set; }
+        public From from { get; set; }
+        public To to { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Cursors
+    {
+        public string before { get; set; }
+        public string after { get; set; }
+    }
+
+    public class Paging
+    {
+        public Cursors cursors { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Messages
+    {
+        public List<Datum3> data { get; set; }
+        public Paging paging { get; set; }
+    }
+
+    public class Datum
+    {
+        public Senders senders { get; set; }
+        public string id { get; set; }
+        public bool can_reply { get; set; }
+        public string scoped_thread_key { get; set; }
+        public Messages messages { get; set; }
+    }
+
+    public class Cursors2
+    {
+        public string before { get; set; }
+        public string after { get; set; }
+    }
+
+    public class Paging2
+    {
+        public Cursors2 cursors { get; set; }
+        public string next { get; set; }
+    }
+
+
+
+    public class SiteConfig
+    {
+        public string username { get; set; }
+        public string userid { get; set; }
+        public string pageid { get; set; }
+        public string data { get; set; }
+        public string id { get; set; }
+        public DateTime? indexdate { get; set; }
+
+        public DateTime? commentdate { get; set; }
+    }
+
+
+}
--- worker/ChatBotWorker/ChatBotWorker/Program.cs
+++ worker/ChatBotWorker/ChatBotWorker/Program.cs
@@ -1,30 +1,127 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace ChatBotWorker
 {
     class Program
     {
+        static void Main2(string[] args)
+        {
+            try
+            {
+                string FBTokens = ConfigurationManager.AppSettings["CHATBOT_FB_TOKEN"];
+            }catch(Exception e)
+            {
+
+            }
+            Console.WriteLine("hihi2");
+
+            Console.ReadKey();
+
+        }
+
+
+        static DateTime LastUpdate = DateTime.Now;
+        public static string FBToken = ConfigurationManager.AppSettings["CHATBOT_FB_TOKEN"];
+
         static void Main(string[] args)
         {
+            GetDataFb(112588937315601);
 
-            Task<bool> obTask = Task.Run(() => (
+            Program.ScheduleTask();
+            //Task<bool> obTask = Task.Run(() => (
 
-                Program.ScheduleTask()
+            //    Program.ScheduleTask()
 
-            ));
+            //));
 
         }
         public static bool ScheduleTask()
         {
             var ListPage = new List<long>();
-            ListPage.Add(124707154243128);
+            ListPage.Add(112588937315601); // MWG BOT
 
+            for (int i = 0; i < 1000000000; i++)
+            {
+
+
+                foreach (var page in ListPage)
+                {
+
+
+                    Task<bool> obTask = Task.Run(() => (
+
+                        Program.GetDataFb(page)
+
+                    ));
+
+                }
+                Thread.Sleep(30000);
+                LastUpdate = DateTime.Now;
+            }
 
             return true;
         }
+
+        public static bool GetDataFb(long page)
+        {
+            WebClient wc = new WebClient();
+            //string token = "EAAUEMk218hkBAD48p9Wp4cEgGeAuq4vLz0PB4NjUi7CfXyRW6yYrlhylUCOai1q9WeBrV7ZBNY4ZAF0tOoYOEUZCZByMMA2HH6Q6WtZBaiJChSsF6xjrLIZAUENlYRyuvRy2ljtAZB1WlDA9PzvVqYkZCx6HZAQKgfP7SoqugurTGXQZDZD";
+
+
+            string token = Program.FBToken;
+            Console.WriteLine("TOKEN: " + token);
+            try
+            {
+                string Url = @"https://graph.facebook.com/v7.0/" + page + "/conversations?fields=senders,id,can_reply,scoped_thread_key,messages.limit(1){message,from}&limit=20&access_token=" + token;// );
+
+                var request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+
+                WebResponse response = request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                GrapAPIModel conc = JsonConvert.DeserializeObject<GrapAPIModel>(responseString);
+                //GrapAPIModel
+                if (conc != null && conc.data != null && conc.data.Count > 0)
+                {
+                    foreach (var iten in conc.data)
+                    {
+
+
+                        if (long.Parse(iten.messages.data[0].from.id) != page)
+                        {
+                            var urll = "http://www.thegioididong.com/qtcm/fb/exeUpdateConversation?conversationid=" + iten.id + "&pageid=" + page;
+                            //string xx = wc.DownloadString(urll);
+
+                            // IMSERT CHAT VÀO MONGODB
+
+                            //if (CmtSvc.InsertArrchiveChat(page, iten.id) > 0)
+                            //{
+                            //    Console.WriteLine("NOTIFY: insert chat new customer\n");
+                            //}
+                        }
+
+
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Loi xay ra:" + e);
+            }
+            return true;
+        }
+
     }
 }
