c211b470984be14ebb6563a87dba2cad24ff6611
Chu Văn Hạnh <vanhanhf5@gmail.com>
2020-11-11 17:16:38 +0700

    up

--- worker/ChatBotWorker/ChatBotWorker/Program.cs
+++ worker/ChatBotWorker/ChatBotWorker/Program.cs
@@ -6,10 +6,14 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Configuration;
-
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace ChatBotWorker
 {
+
+    //https://developers.facebook.com/tools/explorer/652999908626799/?method=GET&path=112588937315601%2Fconversations%3Ffields%3Dsenders%2Cid%2Ccan_reply%2Cscoped_thread_key%2Cmessages.limit(1)%7Bmessage%2Cfrom%7D%26limit%3D50&version=v8.0
+    // 112588937315601/conversations?fields=senders,id,can_reply,scoped_thread_key,messages.limit(1){message,from}&limit=50
     class Program
     {
         static void Main2(string[] args)
@@ -30,9 +34,24 @@ namespace ChatBotWorker
 
         static DateTime LastUpdate = DateTime.Now;
         public static string FBToken = ConfigurationManager.AppSettings["CHATBOT_FB_TOKEN"];
+        public static string MongoUrl = ConfigurationManager.AppSettings["CHATBOT_MONGO_URL"];
+        public static string DBName = ConfigurationManager.AppSettings["CHATBOT_MONGO_DBNAME"];
+        static string CollectionName = "chatThread";
 
+        static MongoClient client = new MongoClient(MongoUrl);
+        static IMongoDatabase database = client.GetDatabase(DBName);
+        static IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>(CollectionName);
         static void Main(string[] args)
         {
+            if(database == null)
+            {
+                database = client.GetDatabase(DBName);
+            }
+            if(collection == null)
+            {
+                collection = database.GetCollection<BsonDocument>(CollectionName);
+            }
+
             GetDataFb(112588937315601);
 
             Program.ScheduleTask();
@@ -99,7 +118,7 @@ namespace ChatBotWorker
 
                         if (long.Parse(iten.messages.data[0].from.id) != page)
                         {
-                            var urll = "http://www.thegioididong.com/qtcm/fb/exeUpdateConversation?conversationid=" + iten.id + "&pageid=" + page;
+                            //var urll = "http://www.thegioididong.com/qtcm/fb/exeUpdateConversation?conversationid=" + iten.id + "&pageid=" + page;
                             //string xx = wc.DownloadString(urll);
 
                             // IMSERT CHAT VÀO MONGODB
@@ -108,6 +127,18 @@ namespace ChatBotWorker
                             //{
                             //    Console.WriteLine("NOTIFY: insert chat new customer\n");
                             //}
+
+                            BsonDocument document = new BsonDocument()
+                                .Add("_id", iten.id) // System.Guid.NewGuid().ToString()
+                                .Add("FbID", iten.id)
+                                .Add("CustomerName", iten.senders.data[0].name)
+                                .Add("LastMessage", iten.messages.data[0].message)
+                                .Add("IsChiem", 0);
+                            
+                            collection.InsertOne(document);
+
+                            //https://laptrinhvb.net/bai-viet/devexpress/---Csharp----Huong-dan-them-xoa-sua-trong-database-MongoDB-/b8ac97af5e995b8b.html
+
                         }
 
 
